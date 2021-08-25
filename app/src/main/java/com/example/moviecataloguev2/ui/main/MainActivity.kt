22bc470e3d4b190a73c0a1fbc85557e107271652
DESKTOP-8BF6OS9\ARDI <wahyuardi027@gmail.com>
2021-08-25 17:01:32 +0700

    bottom navigation

--- app/src/main/java/com/example/moviecataloguev2/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/moviecataloguev2/ui/main/MainActivity.kt
@@ -2,11 +2,40 @@ package com.example.moviecataloguev2.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.widget.Toast
 import com.example.moviecataloguev2.R
+import com.example.moviecataloguev2.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        initUi()
+    }
+
+    private fun initUi() {
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        val menuButton = menu?.findItem(R.id.menuSearch)
+        menuButton?.setOnMenuItemClickListener {
+            if (binding.bnNavigation.currentActiveItemPosition != 1) {
+                binding.bnNavigation.setCurrentActiveItem(1)
+                binding.btSearch.activate()
+            }
+            return@setOnMenuItemClickListener true
+        }
+        return true
     }
 }
